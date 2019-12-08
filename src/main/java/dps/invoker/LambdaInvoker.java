3ba8f68d5e00bed17bfb3433d729e5b774525f1d
Matteo Battaglin <Matteo.Battaglin@Student.uibk.ac.at>
2019-12-08 18:57:15 +0100

    Updated LambdaInvoker

--- src/main/java/dps/invoker/LambdaInvoker.java
+++ src/main/java/dps/invoker/LambdaInvoker.java
@@ -10,7 +10,7 @@ import com.amazonaws.services.lambda.model.InvocationType;
 import com.amazonaws.services.lambda.model.InvokeRequest;
 import com.amazonaws.services.lambda.model.InvokeResult;
 import com.google.gson.Gson;
-
+import dps.invoker.FaaSInvoker;
 import java.util.Map;
 
 /**
@@ -20,15 +20,45 @@ public class LambdaInvoker implements FaaSInvoker {
 
     private String awsAccessKey;
     private String awsSecretKey;
+    private AWSLambda lambda;
 
-    public LambdaInvoker(String awsAccessKey, String awsSecretKey) {
+    
+    /**
+     * Basic Constructor that creates an LambdaInvoker for a specific region with standard settings
+     */
+    public LambdaInvoker(String awsAccessKey, String awsSecretKey, Regions region){
         this.awsAccessKey = awsAccessKey;
         this.awsSecretKey = awsSecretKey;
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setMaxErrorRetry(0);
+        clientConfiguration.setSocketTimeout(900 * 1000);
+        
+        AWSLambda awsLambda = AWSLambdaClientBuilder.standard().withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withClientConfiguration(clientConfiguration)
+                .build();
+        this.lambda = awsLambda;
     }
-
+    
+    /**
+     * Constructor that creates an LambdaInvoker for a specific region with custom ClientConfiguration
+     */
+    public LambdaInvoker(String awsAccessKey, String awsSecretKey, Regions region, ClientConfiguration clientConfiguration) {
+        this.awsAccessKey = awsAccessKey;
+        this.awsSecretKey = awsSecretKey;
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        
+        AWSLambda awsLambda = AWSLambdaClientBuilder.standard().withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withClientConfiguration(clientConfiguration)
+                .build();
+        this.lambda = awsLambda;
+    }
+    
+    
     /**
      * Invokes the lambda function
-     *
      * @param function       function name or ARN
      * @param functionInputs inputs of the function to invoke
      * @return json result in string format
@@ -37,21 +67,9 @@ public class LambdaInvoker implements FaaSInvoker {
         String payload = new Gson().toJson(functionInputs);
         InvokeRequest invokeRequest = new InvokeRequest().withFunctionName(function)
                 .withInvocationType(InvocationType.RequestResponse).withPayload(payload);
-
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-
-        // TODO check: better add this in constructor? Are default values set?
-        ClientConfiguration clientConfiguration = new ClientConfiguration();
-        clientConfiguration.setSocketTimeout(900 * 1000);
-
-        AWSLambda awsLambda = AWSLambdaClientBuilder.standard().withRegion(Regions.US_EAST_1)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                .withClientConfiguration(clientConfiguration)
-                .build();
-
+        
         InvokeResult invokeResult = null;
-
-        invokeResult = awsLambda.invoke(invokeRequest);
+        invokeResult = this.lambda.invoke(invokeRequest);
 
         assert invokeResult != null;
         return new String(invokeResult.getPayload().array());
