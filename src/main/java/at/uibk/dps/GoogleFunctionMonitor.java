9486bd4fd9c5f0217d03f260514b596080f645a9
annakapeller <anna.kapeller@rolmail.net>
2021-03-04 09:40:31 +0100

    throw custom exceptions instead of HTTPResponseexceptions, added comment

--- src/main/java/at/uibk/dps/GoogleFunctionMonitor.java
+++ src/main/java/at/uibk/dps/GoogleFunctionMonitor.java
@@ -1,9 +1,18 @@
 package at.uibk.dps;
 
+import at.uibk.dps.exception.AuthenticationFailedException;
+import at.uibk.dps.exception.MemoryExceededException;
+import at.uibk.dps.exception.TimeLimitException;
 import com.google.api.client.http.HttpResponseException;
 import at.uibk.dps.function.Function;
 import jFaaS.invokers.FaaSInvoker;
 
+
+/**
+ * Method to invoke Google Functions with Monitoring (using jFaas.jar)
+ * Since the GoogleInvoker uses HTTPTriggers, different HTTPResponseException status codes are interpreted
+ * More specific Exceptions from exception package are thrown to be handled down the line
+ */
 public class GoogleFunctionMonitor implements InvokeMonitor{
 
 
@@ -22,26 +31,27 @@ public class GoogleFunctionMonitor implements InvokeMonitor{
 
             if(statusCode == 403){
                 System.out.println("AuthenticationFailedException");
+                throw new AuthenticationFailedException(e.getMessage());
             }
             else if(statusCode== 503){
                 System.out.println("MemoryExceededException");
+                throw new MemoryExceededException(e.getMessage());
 
             }
             else if(statusCode==408){
                 System.out.println("TimeLimitException");
+                throw new TimeLimitException(e.getMessage());
 
             } else{
                 System.out.println("Other HTTPException");
+                throw e;
 
             }
 
         }catch(Exception e){
             System.out.println("Some other Exception");
+            throw e;
         }
-
-
-
-
 
         return returnValue;
 
