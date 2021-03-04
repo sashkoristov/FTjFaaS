6b712440efd47ef54b9a064eff15c0f03a85dae8
annakapeller <anna.kapeller@rolmail.net>
2021-03-04 09:33:20 +0100

    created new exception, covered http exceptions for google monitor, added gradle dependency

--- src/main/java/at/uibk/dps/GoogleFunctionMonitor.java
+++ src/main/java/at/uibk/dps/GoogleFunctionMonitor.java
@@ -1,5 +1,6 @@
 package at.uibk.dps;
 
+import com.google.api.client.http.HttpResponseException;
 import at.uibk.dps.function.Function;
 import jFaaS.invokers.FaaSInvoker;
 
@@ -8,7 +9,42 @@ public class GoogleFunctionMonitor implements InvokeMonitor{
 
     @Override
     public String monitoredInvoke(FaaSInvoker invoker, Function function) throws Exception {
-        return null;
+
+        String returnValue = new String();
+
+
+        try{
+            returnValue= invoker.invokeFunction(function.getUrl(), function.getFunctionInputs()).toString();
+            assert returnValue!= null;
+
+        } catch(HttpResponseException e){
+            int statusCode = e.getStatusCode();
+
+            if(statusCode == 403){
+                System.out.println("AuthenticationFailedException");
+            }
+            else if(statusCode== 503){
+                System.out.println("MemoryExceededException");
+
+            }
+            else if(statusCode==408){
+                System.out.println("TimeLimitException");
+
+            } else{
+                System.out.println("Other HTTPException");
+
+            }
+
+        }catch(Exception e){
+            System.out.println("Some other Exception");
+        }
+
+
+
+
+
+        return returnValue;
+
     }
 
 
